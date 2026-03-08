1f37d3e9cdfc9c6c19b56c73eb884bd10e448592
sai-rohith-bv <venkatsai12234@gmail.com>
2026-03-08 23:12:33 +0530

    Added GET home route (RESTful API) and configured EJS templating for server-side rendering

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const { faker } = require("@faker-js/faker");
 const mysql = require('mysql2');
+const express=require('express');
+const app=express();  
+const path=require('path');
+const port=9090;
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -7,26 +13,6 @@ const connection = mysql.createConnection({
   password:"sai#1234"
 });
  
-let q="INSERT INTO info(username,email,password) values ?";
-
-let data=[];
-for(let  i=0;i<100;i++){
-   data.push(getRandomUser());
-}
-try {
-    connection.query(q,[data],(err,result)=>{
-           if(err){
-            throw err;
-           }
-           else{
-            console.log(result);
-           }
-    });
-}
-    catch(err){
-        console.log(err);
-    }
-connection.end();
 function getRandomUser() {
   return [
      faker.internet.username(),
@@ -35,5 +21,47 @@ function getRandomUser() {
     
   ];
 }
+ app.get("/",(req,res)=>{
+      let q="select count(*) from info";
+      try {
+    connection.query(q,(err,result)=>{
+           if(err){
+            throw err;
+           }
+           else{
+            let count=result[0]["count(*)"];
+            res.render("home",{count});
+           }
+    });
+}
+    catch(err){
+        console.log(err);
+        res.send("Some error in the DB");
+    }
+ })
+ app.listen(9090,()=>{
+  console.log("server is listening on pert 9090....");
+ })
+ 
+//  let q="INSERT INTO info(username,email,password) values ?";
 
+// let data=[];
+// for(let  i=0;i<100;i++){
+//    data.push(getRandomUser());
+// }
+
+ // try {
+//     connection.query(q,[data],(err,result)=>{
+//            if(err){
+//             throw err;
+//            }
+//            else{
+//             console.log(result);
+//            }
+//     });
+// }
+//     catch(err){
+//         console.log(err);
+//     }
+// connection.end();
 // console.log(getRandomUser());
